9320a6ea8479ac0aade296bf324776994a554313
August Klevberg <august.klevberg@student.uib.no>
2020-08-17 16:22:04 +0200

    Changed amount of groups to 26. Made the amount of groups a variable to make it more dynamic. Changed most strings to back-ticks for a cleaner look.

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const client = new Discord.Client();
 
 // The id of the channel where the bot will operate. Change to your needs.
 const bot_channel = "743877236935098388";
-const indent = "  -> "
+const indent = "  ->"
+const groups = 26;
 
-client.once('ready', () => {
-	console.log('Ready!'); // Print "Ready!" when bot is up and running.
+client.once("ready", () => {
+	console.log("Ready!\n"); // Print "Ready!" when bot is up and running.
 });
 
 client.on('message', message => {
@@ -15,29 +16,30 @@ client.on('message', message => {
     if (message.channel.id === bot_channel && !message.member.user.bot) {
 
         const content = message.content;
-        console.log(message.member.displayName + " entered string \"" + content + "\"");
+        console.log(`${message.member.displayName} entered string "${content}"`);
+        // TODO: Add a time and date to the above.
 
         if (!isNaN(content)) { // Check if input is a number.
 
             const number = +content; // Convert string to integer.
-            console.log(indent + content + " IS a valid number. Integer: " + number);
+            console.log(`${indent} ${content} IS a valid number. Integer: ${number}`);
 
-            if (number > 0 && number <= 24) {
+            if (number > 0 && number <= groups) {
 
-                console.log(indent + content + " IS between 1 and 24.");
-                console.log(indent + "Success.\n");
+                console.log(`${indent} ${content} IS between 1 and ${groups}.`);
+                console.log(`${indent} Success.\n`);
 
-                const role_name = "gruppe" + content;
+                const role_name = `gruppe${content}`;
                 const role = message.guild.roles.cache.find(role => role.name === role_name);
                 message.member.roles.add(role); // Assign role to user.
 
             }
             else {
 
-                console.log(indent + content + " is NOT between 1 and 24.");
-                console.log(indent + "Failure.\n");
+                console.log(`${indent} ${content} is NOT between 1 and ${groups}.`);
+                console.log(`${indent} Failure.\n`);
 
-                message.reply("please enter a number between 1 and 24.")
+                message.reply(`please enter a number between 1 and ${groups}.`)
                 .then(msg => {
                     msg.delete({timeout: 10000}) // Delete reply after 10 seconds.
                 })
@@ -47,10 +49,10 @@ client.on('message', message => {
         }
         else {
 
-            console.log(indent + "\"" + content + "\"" + " is NOT a valid number.");
-            console.log(indent + "Failure.\n");
+            console.log(`${indent} "${content}" is NOT a valid number.`);
+            console.log(`${indent} Failure.\n`);
 
-            message.reply("please enter a number between 1 and 24.")
+            message.reply(`please enter a number between 1 and ${groups}.`)
             .then(msg => {
                 msg.delete({timeout: 10000}) // Delete reply after 10 seconds.
             })
